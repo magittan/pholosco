2275e18f466e66ae8ef260e2c0cac95261672df0
Huy <hqh2101@columbia.edu>
2017-11-11 13:51:24 -0500

    first commit

---
+++ methodCall.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+import json
+
+
+apiKey = "c5ffff1a95a4ab5a9e440d76ad56f247"
+api_secret = "1bdc766eee4d312f"
+
+def userIDmod(str):
+	str2 = str.replace("@","%40")
+	return str2
+
+
+def urlconcat(method,bbox):
+	url = "https://api.flickr.com/services/rest/?method=" + method + \
+	"&api_key=c5ffff1a95a4ab5a9e440d76ad56f247&bbox="+ bbox + \
+	"&format=json&nojsoncallback=1"
+	return url
+
+r = urlopen(urlconcat("flickr.photos.search",".skCPTpTVr.Q3WKW"))
+
+text = r.read()
+
+print(text)
