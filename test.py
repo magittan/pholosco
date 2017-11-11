d46c7626edf215043bab7237495732056d7f103a
Huy <hqh2101@columbia.edu>
2017-11-11 06:04:36 -0500

    new website

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-website = requests.get('https://api.flickr.com/services/rest/?method=flickr.photos.getPopular&api_key=c5ffff1a95a4ab5a9e440d76ad56f247')
+website = requests.get('https://api.flickr.com/services')
 
 #what
 print website.status_code
